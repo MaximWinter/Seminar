6db9370127740fce6fd348f664df8ea61894ebec
MaximWinter <winter1.maxim@gmail.com>
2025-05-20 02:32:18 +0200

    a

---
+++ Ohne Titel.R
@@ -0,0 +1,13 @@
+It seems to me that most of those authors from classical to present times who do not accept a frequency interpretation of probability mean something like $\text{probability}_1$ as their explicandum and that their systems themsselves are objectivistic. The latter fact is often veiled by the use of misleading subjectivistic formulations, mostly in preliminary explanations, e.g., in terms of degree of actual or reasonable belief.
+Both concepts are important for science. Many authors who take one of the two concepts as explicandum are not aware of the importance or even the existence of the other concept. This has led to futile controversy.
+$\text{Probability}_2$ is obviously an objective concept. It is important to recognize that $\text{probability}_1$ is likewise objective.
+
+
+
+set.seed(123)
+git
+2 + 2
+
+rnorm(10)
+
+rnorm(15)
